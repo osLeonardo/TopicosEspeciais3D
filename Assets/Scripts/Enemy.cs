5fc33b6e1ac0a7ba4f3eb7a83d24363bb9e91f53
Leonardo Spilere <leonardo.spilere@gmail.com>
2025-05-25 16:43:00 -0300

    animações inimigo finalizadas + correções menores

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,16 +13,20 @@ public class Enemy : MonoBehaviour
     private GameObject _player;
     private static readonly int AttackTrigger = Animator.StringToHash("Attack");
     private static readonly int IsFollowing = Animator.StringToHash("IsFollowing");
+    private bool _isFrozen = false;
+    private Rigidbody _rb;
 
     private void Start()
     {
         _gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         _player = GameObject.FindGameObjectWithTag("Player");
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
+        if (_isFrozen) return;
         var playerPosition = _player.transform.position;
         _navMeshAgent.SetDestination(playerPosition);
         animator.SetBool(IsFollowing, true);
@@ -35,6 +39,15 @@ public class Enemy : MonoBehaviour
 
         animator.SetTrigger(AttackTrigger);
         audioSource.Play();
+        StartCoroutine(HandleAttack());
+    }
+
+    private System.Collections.IEnumerator HandleAttack()
+    {
+        var attackState = animator.GetCurrentAnimatorStateInfo(0);
+        var waitTime = attackState.length;
+        yield return new WaitForSeconds(waitTime);
+
         _gameController.playerLife--;
         _lastAttackTime = Time.time;
         if (_gameController.playerLife <= 0)
@@ -45,5 +58,24 @@ public class Enemy : MonoBehaviour
         {
             _gameController.UpdateLife(_gameController.playerLife);
         }
+    }
+
+    public void FreezeMovement()
+    {
+        _isFrozen = true;
+        if (_navMeshAgent) _navMeshAgent.isStopped = true;
+        if (_rb) _rb.isKinematic = true;
+    }
+
+    public void UnfreezeMovement()
+    {
+        _isFrozen = false;
+        if (_navMeshAgent) _navMeshAgent.isStopped = false;
+        if (_rb) _rb.isKinematic = false;
+    }
+
+    public void FreezeRotation()
+    {
+        if (_rb) _rb.constraints |= RigidbodyConstraints.FreezeRotation;
     }
 }
